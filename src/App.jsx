5daedbd3144f36e0d0bcb71148e184e4dee1570c
Gaikwad Radhika <139673080+Radhika-Gaikwad@users.noreply.github.com>
2024-06-08 21:02:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ScrollIndiator from "./components/custom-scroll-bar"
+
+
+function App() {
+
+
+  return (
+    <>
+     <ScrollIndiator url={"https://dummyjson.com/products?limit=100"}/>
+    </>
+  )
+}
+
+export default App
